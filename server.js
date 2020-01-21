55e90feaa856034f202c2909b102ab436ff6e04f
BigBow <tara.bigbow@gmail.com>
2020-01-20 19:28:19 -0800

    added navbar and setup css

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-'use strict';
+// 'use strict';
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+app.use('/styles',express.static(__dirname +'/styles'));
 const router = express.Router();
 const port = 3000;
+
 
 app.get('/', function(req,res){
  res.sendFile(__dirname + '/main.html');
@@ -25,4 +27,4 @@ app.get('/photography', function(req,res){
 //     })
 // })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`listening on port ${port}!`))
